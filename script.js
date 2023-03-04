0279d75b897eaeaade59ccf7e542a6d33da9bfe7
Praveen Sankhla <praveen.sankhla@e-connectsolutions.com>
2023-03-04 17:12:59 +0530

    Get Full adderess by lat and lng

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function setlocation(txt) {
+    document.getElementById('loc').innerText = txt
+}
+
+    document.getElementById('submit').addEventListener('click', function getlocation() {
+        const d = new Date(); var date = d.toLocaleDateString(); var time = d.toLocaleTimeString();
+
+        if (navigator.geolocation) {
+            let t = navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+
+        async function showPosition(position) {
+            position.coords
+            let lat = position.coords.latitude
+            let longi = position.coords.longitude
+            let url = `https://apis.mapmyindia.com/advancedmaps/v1/560e76f00e788bd6f147373941a7a684/rev_geocode?lat=${lat}&lng=${longi}`
+            
+            const response = await fetch(url);
+            var data = await response.json();
+            console.log(data.results[0])
+            
+            let pk = `${data.results[0].formatted_address}`
+            setlocation(pk)
+        }
+    })
+
+   
